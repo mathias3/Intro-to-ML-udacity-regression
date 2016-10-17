b8e6994e019631b5cdad1eb3f827c9b44c3f549d
mathias3 <maciej.osowski@gmail.com>
2016-10-17 22:28:45 +0100

    Create finance regression.py

---
+++ finance regression.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+"""
+    Starter code for the regression mini-project.
+    
+    Loads up/formats a modified version of the dataset
+    (why modified?  we've removed some trouble points
+    that you'll find yourself in the outliers mini-project).
+
+    Draws a little scatterplot of the training/testing data
+
+    You fill in the regression code where indicated:
+"""    
+
+
+import sys
+import pickle
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
