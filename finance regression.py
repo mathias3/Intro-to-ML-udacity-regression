0ee2bbe4d20217cf0a94ee22c73cb197259b855d
mathias3 <maciej.osowski@gmail.com>
2016-10-17 22:29:22 +0100

    Update finance regression.py

--- finance regression.py
+++ finance regression.py
@@ -18,3 +18,37 @@ import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+
+### list the features you want to look at--first item in the 
+### list will be the "target" feature
+features_list = ["bonus", "salary"]
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+target, features = targetFeatureSplit( data )
+
+### training-testing split needed in regression, just like classification
+from sklearn.cross_validation import train_test_split
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+train_color = "b"
+test_color = "r"
+
+
+
+### Your regression goes here!
+### Please name it reg, so that the plotting code below picks it up and 
+### plots it correctly. Don't forget to change the test_color above from "b" to
+### "r" to differentiate training points from test points.
+
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit( feature_test, target_test)
+
+
+
+features_list = ["bonus", "long_term_incentive"]
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print ("slope:", reg.coef_[0])
+print ("intercept:", reg.intercept_)
+print ("r-squared score:", reg.score(feature_test, target_test))
+
+
